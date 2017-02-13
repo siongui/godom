c69c8f22c6cb099be60c0221accba41cad292daf
siongui <siongui@gmail.com>
2017-02-13 23:19:09 +0800

    key and keyCode of KeyboardEvent

---
+++ keyboardevent.go
@@ -0,0 +1,18 @@
+// This file implements KeyboardEvent interface
+// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+package godom
+
+// Properties
+
+// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/key
+func (e Event) Key() string {
+	return e.Get("key").String()
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode
+// MDN says keyCode is deprecated, use key instead.
+func (e Event) KeyCode() int {
+	return e.Get("keyCode").Int()
+}
+
+// Methods
