4742c043582577e8d9463ab5c12d2b1ed7bc6a20
siongui <siongui@gmail.com>
2017-03-16 06:18:38 +0800

    add element blur

--- element.go
+++ element.go
@@ -19,6 +19,12 @@ func (o *Object) SetInnerHTML(html string) {
 
 // Methods
 
+// Remove keyboard focus from the current element
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/blur
+func (o *Object) Blur() {
+	o.Call("blur")
+}
+
 // Set focus on the specified element, if it can be focused.
 // https://developer.mozilla.org/en/docs/Web/API/HTMLElement/focus
 func (o *Object) Focus() {
